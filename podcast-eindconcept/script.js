d64140a8ed9fa95e9c2021f8a448790a056473e1
sanneduinkerx <sanne.duinker@hva.nl>
2021-04-29 10:09:28 +0200

    dark mode + font size

--- podcast-eindconcept/script.js
+++ podcast-eindconcept/script.js
@@ -1,16 +1,48 @@
 const content = document.querySelector('.content p')
 const section = document.querySelector('.content')
+const tune = document.querySelector('body .tune');
+const body = document.querySelector('body');
+const fontSize = document.getElementById('fontSize');
+const interlinie = document.getElementById('interlinie');
+const valueFont = document.getElementById('value1');
+const valueLine = document.getElementById('value2');
+const transcriptText = document.querySelectorAll('main section:nth-of-type(2) ul li p');
+const buttonOption = document.getElementsByClassName('option')
+const header = document.querySelector('header');
+const darkMode = document.getElementsByClassName('darkMode');
+
+
+buttonOption[0].addEventListener('click', () => {
+    console.log('clicked')
+    header.classList.toggle('optionsOn')
+})
+
+fontSize.addEventListener('input', () => {
+    console.log(fontSize.value)
+    valueFont.textContent = fontSize.value;
+    transcriptText.forEach(text => {
+        text.style.fontSize = `${fontSize.value}px`;
+    })
+})
+
+interlinie.addEventListener('input', () => {
+    valueLine.textContent = interlinie.value;
+    transcriptText.forEach(text => {
+        text.style.lineHeight = `${interlinie.value}px`;
+    })
+})
+
+darkMode[0].addEventListener('click', () => {
+    body.classList.toggle('darkModeActivated')
+})
+
+
 
 if(content){
     content.addEventListener('click', function showContent(){
         section.classList.toggle('show');
     })
 }
-
-
-const tune = document.querySelector('body .tune');
-const body = document.querySelector('body');
-
 
 if(tune){
     tune.addEventListener('click', function disco(){
@@ -20,7 +52,5 @@ if(tune){
         setTimeout(function(){ 
             body.classList.remove('DISCO');
         }, 2000);
-
-        
     })
 }
